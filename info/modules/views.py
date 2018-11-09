44cf31f5d909c7a339cda8dfc5843b6fe65ae615
lnsist <lnsist@yeah.net>
2018-11-09 08:59:16 +0800

    网站小图标

--- info/modules/views.py
+++ info/modules/views.py
@@ -3,7 +3,7 @@
 date: 18-11-8 下午8:24
 """
 import logging
-from flask import render_template
+from flask import render_template, current_app
 from . import index_blu
 
 
@@ -16,3 +16,10 @@ def index():
     """
     logging.debug("调试日志")
     return render_template("news/index.html")
+
+# 定义路由函数 -- 必定是此路由, 请求网站小图标
+@index_blu.route('/favicon.ico')
+def favicon():
+    # 返回静态文件小图标 -- app上下文发送静态文件
+    return current_app.send_static_file('news/favicon.ico')
+
