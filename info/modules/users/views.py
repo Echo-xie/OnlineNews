64a93ec4ff413f39e3c8d47524999cf564e68674
lnsist <lnsist@yeah.net>
2018-11-15 21:17:02 +0800

    新增 个人中心 -- 密码修改

--- info/modules/users/views.py
+++ info/modules/users/views.py
@@ -16,10 +16,12 @@ def info():
         用户信息
     :return:
     """
-    data = {
-        "user": g.user.to_dict() if g.user else None
-    }
-    return render_template("users/user_info.html", data=data)
+    # data = {
+    #     "user": g.user.to_dict() if g.user else None
+    # }
+    # return render_template("users/user_info.html", data=data)
+    # 封装功能同上
+    return users_render_template("users/user_info.html")
 
 
 @users_blu.route("/user_base_info", methods=["POST", "GET"])
@@ -31,42 +33,44 @@ def user_base_info():
     user = g.user
     # 请求方式 == get
     if request.method == "GET":
-        # 封装数据
-        data = {
-            "user": user.to_dict() if user else None
-        }
-        # 返回
-        return render_template("users/user_base_info.html", data=data)
-    else:
-        """post提交数据"""
-        # 获取请求体
-        data_dict = request.json
-        # 个性签名
-        signature = data_dict.get("signature")
-        # 用户名称
-        nick_name = data_dict.get("nick_name")
-        # 性别
-        gender = data_dict.get("gender")
-        # 判断参数是否都有数据
-        if not all([nick_name, gender]):
-            return jsonify(errno=RET.PARAMERR, errmsg="参数不齐")
-        # 判断参数数据是否正确
-        if gender not in ["MAN", "WOMAN"]:
-            return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
-        # 赋值
-        user.signature = signature
-        user.nick_name = nick_name
-        user.gender = gender
-        try:
-            # 事务提交
-            mysql_db.session.commit()
-        except Exception as e:
-            current_app.logger.error(e)
-            return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
-        # 修改session属性
-        session["nick_name"] = nick_name
-        # 返回
-        return jsonify(errno=RET.OK, errmsg="更新成功")
+        # # 封装数据
+        # data = {
+        #     "user": user.to_dict() if user else None
+        # }
+        # # 返回
+        # return render_template("users/user_base_info.html", data=data)
+        # 封装功能同上
+        return users_render_template("users/user_base_info.html")
+    """post提交数据"""
+    # 获取请求体
+    data_dict = request.json
+    # 个性签名
+    signature = data_dict.get("signature")
+    # 用户名称
+    nick_name = data_dict.get("nick_name")
+    # 性别
+    gender = data_dict.get("gender")
+    # 判断参数是否都有数据
+    if not all([nick_name, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不齐")
+    # 判断参数数据是否正确
+    if gender not in ["MAN", "WOMAN"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    # 赋值
+    user.signature = signature
+    user.nick_name = nick_name
+    user.gender = gender
+    try:
+        # 事务提交
+        mysql_db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        mysql_db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
+    # 修改session属性
+    session["nick_name"] = nick_name
+    # 返回
+    return jsonify(errno=RET.OK, errmsg="更新成功")
 
 
 @users_blu.route("/follow", methods=['POST'])
@@ -119,3 +123,53 @@ def follow():
         return jsonify(errno=RET.DBERR, errmsg="数据库保存出错")
     # 成功返回
     return jsonify(errno=RET.OK, errmsg="操作成功")
+
+
+@users_blu.route("/user_pass_info", methods=["GET", "POST"])
+def user_pass_info():
+    """
+        密码修改展示, 和修改密码
+    :return:
+    """
+    # 获取登陆用户
+    user = g.user
+    # 请求方式 == get
+    if request.method == "GET":
+        # 返回模板
+        return users_render_template("users/user_pass_info.html")
+    # 获取请求体
+    data_dict = request.json
+    # 原密码
+    old_password = data_dict.get("old_password")
+    # 新密码
+    new_password = data_dict.get("new_password")
+    # 判断参数是否都有数据
+    if not all([old_password, new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不齐")
+    # 判断原密码是否正确
+    if not user.check_passowrd(old_password):
+        return jsonify(errno=RET.PWDERR, errmsg="原密码错误")
+    # 保存 密码
+    user.password = new_password
+    try:
+        # 事务提交
+        mysql_db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        mysql_db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
+    # 返回
+    return jsonify(errno=RET.OK, errmsg="密码修改成成功")
+
+
+def users_render_template(template):
+    """
+        用户蓝图模块返回
+    :param template:
+    :return:
+    """
+    # 封装数据
+    data = {
+        "user": g.user.to_dict() if g.user else None
+    }
+    return render_template(template, data=data)
--- info/static/news/js/user_pass_info.js
+++ info/static/news/js/user_pass_info.js
@@ -2,28 +2,33 @@ $(function () {
     $(".pass_info").submit(function (e) {
         // 阻止表单的默认提交行为
         e.preventDefault();
-
+        var error_tip = $(".error_tip");
         var old_password = $('#old_password').val();
         var new_password = $('#new_password').val();
         var new_password2 = $('#new_password2').val();
 
+        error_tip.hide()
         if (!old_password) {
-            alert('原密码不能为空!');
+            error_tip.html("原密码不能为空!")
+            error_tip.show()
             return;
         }
 
         if (!new_password) {
-            alert('新密码不能为空!');
+            error_tip.html("新密码不能为空!")
+            error_tip.show()
             return;
         }
 
         if (!new_password2) {
-            alert('重复新密码不能为空!');
+            error_tip.html("重复新密码不能为空!")
+            error_tip.show()
             return;
         }
 
         if (new_password != new_password2) {
-            alert('两次密码不一致!');
+            error_tip.html("两次密码不一致!")
+            error_tip.show()
             return;
         }
 
@@ -32,8 +37,44 @@ $(function () {
             "old_password": old_password,
             "new_password": new_password
         };
-        
-        // TODO: 请求修改用户密码
 
+        // TODO: 请求修改用户密码
+        init_ajax("/users/user_pass_info", params, function (resp) {
+            if (resp.errno == "0") {
+                alert("更新成功!")
+                window.location.reload()
+            } else {
+                error_tip.html(resp.errmsg)
+                error_tip.show()
+                alert(resp.errmsg)
+            }
+        })
     })
 });
+
+// 获取cookie
+function getCookie(name) {
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : undefined;
+}
+
+// 封装ajax请求
+function init_ajax(out_url, params, callback_success) {
+    $.ajax({
+        url: out_url,
+        type: "POST",
+        contentType: "application/json",
+        headers: {
+            "X-CSRFToken": getCookie("csrf_token")
+        },
+        data: JSON.stringify(params),
+        dataType: "json",
+        success: function (resp) {
+            if (resp.errno == 4104) {
+                $('.login_form_con').show();
+                return false;
+            }
+            callback_success(resp)
+        }
+    });
+}
