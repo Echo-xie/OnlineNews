fea06047f706e973ff7ee06a3a79594250f6cd44
lnsist <lnsist@yeah.net>
2018-11-16 10:48:57 +0800

    优化代码, 个人中心 -- 新闻发布

--- info/static/news/js/user_news_release.js
+++ info/static/news/js/user_news_release.js
@@ -6,15 +6,17 @@ $(function () {
 
         // TODO 发布完毕之后需要选中我的发布新闻
         $(this).ajaxSubmit({
-            // beforeSubmit: function (request) {
-            //     // 在提交之前，对参数进行处理
-            //     for (var i = 0; i < request.length; i++) {
-            //         var item = request[i];
-            //         if (item["name"] == "content") {
-            //             item["value"] = tinyMCE.activeEditor.getContent();
-            //         }
-            //     }
-            // },
+            beforeSubmit: function (request) {
+                // 在提交之前，对参数进行处理
+                for (var i = 0; i < request.length; i++) {
+                    var item = request[i];
+                    // 获取参数名为 content
+                    if (item["name"] == "content") {
+                        // 参数数据 转换 类型
+                        item["value"] = tinyMCE.activeEditor.getContent();
+                    }
+                }
+            },
             url: "/users/user_news_release",
             type: "POST",
             headers: {
@@ -27,7 +29,7 @@ $(function () {
                     // 滚动到顶部
                     window.parent.scrollTo(0, 0)
 
-                }else {
+                } else {
                     alert(resp.errmsg)
                 }
             }
